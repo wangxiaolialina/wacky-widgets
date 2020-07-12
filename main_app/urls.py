830cc3ff80e8fa944a1f929590a6b2951dea6abb
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-12 13:17:47 -0400

    add and delete widget url link

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add_widget', views.add_widget, name='add_widget'),
+    path('<int:pk>/delete/', views.WidgetDelete.as_view(), name='widget_delete')
 ]
