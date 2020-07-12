2fa4ed375935c2f7235a93a8342e1bfd6ee77d1e
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-12 11:07:43 -0400

    add index function

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Widget
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    widget_list = Widget.objects.all()
+    return render(request, 'home.html',{widget_list:widget_list})
